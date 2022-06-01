1747b47e93b6db1cdbeda07d4835caf2d37b1bbb
Erekle Tvinadze <Erekle_Tvinadze@epam.com>
2022-06-01 18:09:28 +0400

    add logic for slice

--- task02-slice.go
+++ task02-slice.go
@@ -1,6 +1,14 @@
 package homework
 
 func reverse(input []int64) (result []int64) {
-	//Place your code here
-	return
+	inputLen := len(input)
+	result = make([]int64, inputLen)
+
+	for i, n := range input {
+		j := inputLen - i - 1
+
+		result[j] = n
+	}
+
+	return result
 }
