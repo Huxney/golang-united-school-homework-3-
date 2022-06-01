50c845d8b685f05a094d66841daa44ae801f03a0
Erekle Tvinadze <Erekle_Tvinadze@epam.com>
2022-06-01 18:54:26 +0400

    add logic for map

--- task03-map.go
+++ task03-map.go
@@ -1,6 +1,20 @@
 package homework
 
+import (
+	"sort"
+)
+
 func sortMapValues(input map[int]string) (result []string) {
-	//Place your code here
-	return
+	keys := make([]int, len(input))
+	i := 0
+	for k := range input {
+		keys[i] = k
+		i++
+	}
+	sort.Ints(keys)
+	for _, k := range keys {
+		result = append(result, input[k])
+	}
+
+	return result
 }
